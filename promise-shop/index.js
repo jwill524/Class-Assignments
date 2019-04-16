5cebba216c190040945e2d96bc49586a328db6ee
Jonathan Williams <jonathanwilliams96@hotmail.com>
2019-04-16 11:50:22 -0400

    update

--- promise-shop/index.js
+++ promise-shop/index.js
@@ -1,1 +1,14 @@
+const moment = require('moment');
+const request = require('request-promise')
 console.log("hfuapidsg")
+
+
+
+    setInterval(() => {
+        request('http://jsonplaceholder.typicode.com/posts/1')
+    .then(console.log)
+
+   var day = moment().format('MMMM Do YYYY, h:mm:ss a');
+    console.log(day)
+        
+    }, 3000);
